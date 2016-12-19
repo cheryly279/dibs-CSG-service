f8dbc4f43f982e21ddef5c7c37ebf40e9cd3465e
cheryly279 <cheryl.yaeger@gmail.com>
2016-12-19 16:22:53 -0500

    Fixing CORS

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 const connection = mysql.createConnection({
     host: 'coding-standard-digest.c0krrqwzvwip.us-east-1.rds.amazonaws.com',
